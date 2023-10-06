e1cf6d0a10fb737c580a328db34bc760accce7bd
Ko Jae Sung <fortear@naver.com>
2023-10-07 01:11:21 +0900

    Update zigi-bedrock2-1-embedding.py

--- zigi-bedrock2-1-embedding.py
+++ zigi-bedrock2-1-embedding.py
@@ -15,27 +15,27 @@ from langchain.embeddings import BedrockEmbeddings
 os.environ["AWS_DEFAULT_REGION"] = "us-east-1"  
 os.environ["AWS_PROFILE"] = "zigi-bedrock"
 
-# PDF 파일 경로 목록
-pdf_files = [
-    "./data/visa-kor.pdf",
-    "./data/card.pdf",
-    "./data/network.pdf"
-]
-
-# 로더, 텍스트 분할기 및 임베딩 초기화
-text_splitter = CharacterTextSplitter(chunk_size=3000, chunk_overlap=600)
-
 boto3_bedrock = bedrock.get_bedrock_client(
     # IAM User에 Bedrock에 대한 권한이 없이 Role을 Assume하는 경우
     # assumed_role=os.environ.get("BEDROCK_ASSUME_ROLE", None), 
     region=os.environ.get("AWS_DEFAULT_REGION", None)
 )
 
+#Embedding 모델 정의
 bedrock_embeddings = BedrockEmbeddings(client=boto3_bedrock, model_id="amazon.titan-embed-text-v1")
+
+pdf_files = [
+    "./data/visa-kor.pdf",
+    "./data/card.pdf",
+    "./data/network.pdf"
+]
+
+# Chunking을 위한 Text Splitter 설정 : Spiltter
+text_splitter = CharacterTextSplitter(chunk_size=3000, chunk_overlap=600)
 
 texts = []
 
-# 각 PDF 파일에 대해 작업 수행
+#문서 Chunking
 for pdf_file in pdf_files:
     loader = PyPDFLoader(pdf_file)
     documents = loader.load()
